ab530ad5db8b84d1261f55f7814418c0520ca80f
Tarkan Kara <unnahmet23@hotmail.com>
2023-04-06 10:38:12 +0300

    Updated

---
+++ bin/4_late_keyword/example8.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  print("late fullName is being initialized");
+  late final fullName = getFullName();
+  // print("after");
+  // print(fullName);
+  final resolvedFullName = fullName;
+  print("resolvedFullName = $resolvedFullName");
+}
+
+String getFullName() {
+  print("getFullName() is called");
+  return "John Doe";
+}
+
+// late fullName is being initialized
+// getFullName() is called
+// resolvedFullName = John Doe
