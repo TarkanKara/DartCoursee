ed076d4dc7356f99095fed02ff0b66e33f76d884
Tarkan Kara <unnahmet23@hotmail.com>
2023-06-02 00:01:42 +0300

    updated

--- bin/7_collections/example2.dart
+++ bin/7_collections/example2.dart
@@ -21,4 +21,33 @@ void main(List<String> args) {
   print(uniqueSurnames);
 
   print("----------------------------------");
+
+  final little = "Little";
+  final little2 = "Little";
+  print(little.hashCode);
+  print(little2.hashCode);
+
+  print("----------------------------------");
+
+  if (surnames.contains("Schamberger")) {
+    print("Yes");
+  } else {
+    print("No");
+  }
+
+  print("----------------------------------");
+
+  // final ages1 = [20, 30, 40];
+  // final ages2 = [20, 30, 40];
+
+  final ages1 = {20, 30, 40};
+  final ages2 = {20, 30, 40};
+
+  if (ages1 == ages2) {
+    print("Ages are equal");
+  } else {
+    print("Ages are not equal");
+  }
+
+  print("----------------------------------");
 }
